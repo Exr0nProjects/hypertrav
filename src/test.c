64f08a2792da8ba327c2c488331eaa076c6ef522
Exr0n <mail@exr0n.com>
2021-03-21 20:24:49 -0700

    lockless stacks are hard
    dont even understand atomic numbers

--- src/test.c
+++ src/test.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
+const size_t MAX_PATH_LEN = 4096;
+size_t threads = 1;             // this shouldn't be changed once threads are allocated
+char **bufs = NULL;
+
 // stack
+int * _Atomic stack_store;
+int _Atomic stack_sz;        // number of elements on the stack
+int _Atomic stack_opcnt;
+
+void st_push()
+{
+	printf("%u\n", __atomic_load_n(&stack_sz, __ATOMIC_RELAXED));
+	for (;;) {
+	}
+}
+char *st_pop()
+{
+	printf("%u\n", __atomic_load_n(&stack_sz, __ATOMIC_RELAXED));
+}
 
 // thread functions
 void *threaded(void *var)
@@ -14,6 +33,14 @@ void *threaded(void *var)
 // main
 int main()
 {
+	// set up datastructures
+	const size_t input = 3;
+	threads = input;
+	stack_store = calloc(threads, sizeof(size_t));
+
+	st_pop();
+
+	// threading stuffs
 	pthread_t thread_id;
 	puts("starting thread");
 	pthread_create(&thread_id, NULL, threaded, NULL);
